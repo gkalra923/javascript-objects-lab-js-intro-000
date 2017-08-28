c32d4bf777ef2b460d93f2fbeb50ea5350a3e365
giri kalra <giiirrriii@gmail.com>
2017-08-28 20:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value ) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = {};
-  newObj = object;
+  var newObj = Object.assign({}, object);
   delete newObj[key];
-  return newObj;
+  return object;
 }
